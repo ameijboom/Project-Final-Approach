de4c2eed26c483995c06b6d967fd0dca030fee7b
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2022-05-12 23:17:06 +0200

    Added a timer

--- PFA/MyGame/Models/Game/Timer.cs
+++ PFA/MyGame/Models/Game/Timer.cs
@@ -1,13 +1,18 @@
 using PFA.GXPEngine;
 using PFA.GXPEngine.AddOns;
+using PFA.GXPEngine.Utils;
 
 namespace PFA.MyGame.Models.Game
 {
 	public class Timer : Sprite
 	{
+		private readonly int _millisAtStartRound;
+		private const int MAX_MILLIS = 5 * 60 * 1000;
+
 		public Timer(TiledObject obj = null) : base("./assets/UI/timer.png", addCollider: false)
 		{
 			// game.AddChild(this);
+			_millisAtStartRound = Time.time;
 		}
 
 		public void Update()
@@ -15,7 +20,26 @@ namespace PFA.MyGame.Models.Game
 			float xPos = x - 10;
 			float yPos = y - 16;
 			// Gizmos.DrawCircle(xPos, yPos, 8, colour:Colour.Green);
-			MyGame.Text("5:00", xPos, yPos); //TODO: change to actual timer
+			MyGame.Text(GetFormattedTime(MAX_MILLIS - GetTimeInMillis()), xPos, yPos);
+
+		}
+
+		private int GetTimeInMillis()
+		{
+			return Time.time - _millisAtStartRound;
+		}
+
+		private static string GetFormattedTime(int millis)
+		{
+			int i = millis / 1000;
+			int minutes = i / 60;
+			int seconds = i % 60;
+			return minutes + ":" + seconds.ToString("00");
+		}
+
+		public bool IsTimeUp()
+		{
+			return GetTimeInMillis() >= MAX_MILLIS;
 		}
 	}
 }
